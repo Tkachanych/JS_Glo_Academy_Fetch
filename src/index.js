d22fd4f38fd1e1e35353881d31702db7801f91c8
Vladimir Tkachenko <geletar@gmail.com>
2023-03-05 15:34:41 +0400

    lesson26 XMLHttpRequest

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const file = './db/db.json';
 const site = 'https://jsonplaceholder.typicode.com/posts';
-const headers = { 'Content-type': 'application/json; charset=UTF-8' };
 
-const sendData = (site, data, headers) => {
-  console.log(data);
+const sendData = (site, data) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', site, true);
+  xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+  xhr.send(JSON.stringify(data));
+
+  xhr.onerror = function () {
+    console.log('Запрос не удался');
+  };
+
+  xhr.onload = function () {
+    if (xhr.status >= 400 || xhr.status === 0) {
+      console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+      return;
+    }
+    console.log(JSON.parse(xhr.response));
+  };
 };
 
 const getData = (source) => {
@@ -14,7 +28,7 @@ const getData = (source) => {
       }
       return Promise.reject(response);
     })
-    .then((data) => sendData(site, data, headers))
+    .then((data) => sendData(site, data))
     .catch((err) => console.log(err));
 };
 
